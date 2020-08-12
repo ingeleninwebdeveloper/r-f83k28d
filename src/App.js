ff398f38c331c9d18e35a10b05b134f52b632d0f
Lenin fernando <ingeleninfernando@gmail.com>
2020-08-12 09:44:32 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function prueba() {
+
+  let valor = 0
+return valor;
+}
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state={
+      count:0
+    }
+  }
+
+  onclick(type){
+      this.setState(prevState => {
+         return {count: type == 'add' ? prevState.count + 1: prevState.count - 1}
+      });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value" id="prueba">{this.state.count}</span>
+        <button id="inc" onClick={this.onclick.bind(this, 'add')}>Incrementa</button>
       </div>
     );
   }
+
+
 }
+
 
 export default App;
